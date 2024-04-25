6a4db1a69facedcd68ccb3247444b5402ec09d14
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-04-25 19:07:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,52 +8,46 @@ import Notification from "./components/notification/Notification"
 function App() {
 
   const [feedback, setFeedback] = useState(() => {
-    const savedFeedback = localStorage.getItem("saved-feedback")
-    if (savedFeedback !== null) {
-      console.log(savedFeedback);
-      return JSON.parse(savedFeedback);
-    } else {
-      ({ "good": 0, "neutral": 0, "bad": 0, })
+    const savedFeedback = localStorage.getItem("saved-feedback");
+     return savedFeedback ? JSON.parse(savedFeedback) : ({ "good": 0, "neutral": 0, "bad": 0 })
+  });
+    useEffect(() => {
+      localStorage.setItem("saved-feedback", JSON.stringify(feedback))
+    },)
+    const updateFeedback = (feedbackType) => {
+      setFeedback((prevFeedback) => ({
+        ...feedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1
+      })
+      )
     }
-  })
-  useEffect(() => {
-    localStorage.setItem("saved-feedback", JSON.stringify(feedback))
-  },)
-  console.log({ feedback });
-  const updateFeedback = (feedbackType) => {
-    setFeedback((prevFeedback)=>({
-      ...feedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1
-    })
-    )
-  }
-  const handleResetFeedback = () => {
-    setFeedback(f => ({
-      ...f,
-      "good": 0,
-      "neutral": 0,
-      "bad": 0,
-    }))
-  }
- 
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+    const handleResetFeedback = () => {
+      setFeedback(f => ({
+        ...f,
+        "good": 0,
+        "neutral": 0,
+        "bad": 0,
+      }))
+    }
+    const { good, neutral, bad } = feedback;
+    const totalFeedback = good + neutral + bad;
 
 
-  return (
-    <>
-      <Description />
-      <Options
-        updateFeedback={updateFeedback}
-        totalFeedback={totalFeedback}
-        resetFeedback={handleResetFeedback}
-      />
-      {totalFeedback > 0 ? (
-        <Feedback feedbackData={feedback} totalFeedback={totalFeedback} />
-      ) : (
-        <Notification />
-      )}
-    </>
-  );
-}
+    return (
+      <>
+        <Description />
+        <Options
+          updateFeedback={updateFeedback}
+          totalFeedback={totalFeedback}
+          resetFeedback={handleResetFeedback}
+        />
+        {totalFeedback > 0 ? (
+          <Feedback feedbackData={feedback} totalFeedback={totalFeedback} />
+        ) : (
+          <Notification />
+        )}
+      </>
+    );
+  }
 
 export default App
