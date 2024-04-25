15f2306fce056a6d9398dfe96e688d5dc773b0f0
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-04-25 20:16:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   });
     useEffect(() => {
       localStorage.setItem("saved-feedback", JSON.stringify(feedback))
-    },)
+    }, [feedback])
     const updateFeedback = (feedbackType) => {
       setFeedback((prevFeedback) => ({
         ...feedback,
